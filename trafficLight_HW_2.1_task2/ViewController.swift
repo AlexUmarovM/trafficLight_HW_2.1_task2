b5c164eb261ae81784d0188ec3544bf3454e2490
Alex Umarov <umarov.iosdev@gmail.com>
2020-05-19 21:22:02 +0300

    Initial Commit

---
+++ trafficLight_HW_2.1_task2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trafficLight_HW_2.1_task2
+//
+//  Created by Александр Умаров on 19.05.2020.
+//  Copyright © 2020 Александр Умаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
