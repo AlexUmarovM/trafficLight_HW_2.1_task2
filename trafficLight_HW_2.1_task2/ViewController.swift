711eede72da1e68d22cee8f7da69c58a0e0ee58f
Alex Umarov <umarov.iosdev@gmail.com>
2020-05-19 23:47:30 +0300

    Working Switch for traffic light

--- trafficLight_HW_2.1_task2/ViewController.swift
+++ trafficLight_HW_2.1_task2/ViewController.swift
@@ -13,31 +13,41 @@ class ViewController: UIViewController {
     @IBOutlet var yellowLightView: UIView!
     @IBOutlet var greenLightView: UIView!
     @IBOutlet var startButton: UIButton!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         redLightView.alpha = 0.2
         yellowLightView.alpha = 0.2
         greenLightView.alpha = 0.2
+        
         redLightView.layer.cornerRadius = 65
         yellowLightView.layer.cornerRadius = 65
         greenLightView.layer.cornerRadius = 65
         
     }
-    var glowingLight =
+    var glowingLight: UIView!
     @IBAction func changeLight() {
-    
-        switch glowingLight {
+        switch  glowingLight {
+        case nil:
+            greenLightView.alpha = 0.2
+            startButton.setTitle("START", for: .normal)
+            glowingLight = redLightView
+            
         case redLightView:
             redLightView.alpha = 1
             startButton.setTitle("NEXT", for: .normal)
             glowingLight = yellowLightView
+        
         case yellowLightView:
             redLightView.alpha = 0.2
             yellowLightView.alpha = 1
             glowingLight = greenLightView
+        
         case greenLightView:
             yellowLightView.alpha = 0.2
             greenLightView.alpha = 1
+            glowingLight = nil
+        
         default:
             break
           
